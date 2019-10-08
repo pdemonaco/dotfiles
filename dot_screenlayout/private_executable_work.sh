ccb2756bd66fb5301ff1d71040b696db835a8697
Phil DeMonaco <pdemon@gmail.com>
2019-10-08 11:38:41 -0400

    maint: update work display settings
    Turns out I didn't have intel drivers actually installed...

--- dot_screenlayout/private_executable_work.sh
+++ dot_screenlayout/private_executable_work.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1080 --pos 0x360 --rotate normal --output HDMI-3 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-3 --off --output DP-2 --off --output DP-1 --primary --mode 2560x1440 --pos 1920x0 --rotate normal
-sleep 1
-nitrogen --restore
+xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --mode 1920x1080 --pos 0x360 --rotate normal --output DP1 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
