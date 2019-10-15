870d4b4982559e407b7ec9c899f970a9a9720cd8
Phil DeMonaco <pdemon@gmail.com>
2019-10-15 12:08:46 -0400

    maint: update attached monitor config
    Changed because I actually have a driver installed now..

--- dot_screenlayout/private_executable_attached_monitor.sh
+++ dot_screenlayout/private_executable_attached_monitor.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output DP-2-1 --off --output DP-2-8 --off --output eDP-1 --primary --mode 1920x1080 --pos 5120x360 --rotate normal --output HDMI-3 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off
+xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
 sleep 1
 nitrogen --restore
