377f45d9b2fe58f3a5cb3314e345ff32b0622126
Phil DeMonaco <pdemon@gmail.com>
2020-02-01 12:12:29 -0500

    fix: remove invalid command
    The reference I stole this from was importing some other module and
    removing their alias for touch. Since I'm not doing that I don't need
    this in my life...

--- private_Documents/PowerShell/Profile.ps1
+++ private_Documents/PowerShell/Profile.ps1
@@ -64,7 +64,7 @@ function pgrep($name) {
 
 # Like Unix touch, creates new files and updates time on old ones
 # PSCX has a touch, but it doesn't make empty files
-Remove-Alias touch
+#Remove-Alias touch
 function touch($file) {
 	if ( Test-Path $file ) {
 		Set-FileTime $file
