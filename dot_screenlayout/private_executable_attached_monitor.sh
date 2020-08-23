f92bb749d934b008c3e97ae341003b6f201b3f80
Philip DeMonaco <phil@demona.co>
2020-08-22 23:35:25 -0400

    feat: swap to nvidia driver in xorg
    Changes all the names of my monitors. Also has requirements in xinitrc
    to ensure the source is actually working.

--- dot_screenlayout/private_executable_attached_monitor.sh
+++ dot_screenlayout/private_executable_attached_monitor.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+xrandr --output DP-0.2 --off --output DP-0.3 --off --output DP-0 --off --output DP-1 --off --output DP-2 --off --output HDMI-0 --off --output DP-3 --off --output eDP-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 sleep 1
 nitrogen --restore
--- dot_screenlayout/private_executable_home.sh
+++ dot_screenlayout/private_executable_home.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --off --output DP1 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --primary --mode 2560x1440 --pos 2560x0 --rotate normal
+xrandr --output DP-0.2 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-0.3 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --off --output HDMI-0 --off --output DP-3 --off --output eDP-1-1 --off
 sleep 1
 nitrogen --restore
