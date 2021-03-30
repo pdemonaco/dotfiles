f303e881238e53016f551764d6d1952b9da28777
Philip DeMonaco <phil@demona.co>
2021-03-30 13:27:20 -0400

    feat: conversion to nouveau and some other shit

---
+++ dot_screenlayout/private_executable_17_home-nouveau-only.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr --output eDP-1 --off --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-1-1 --off --output DP-1-2 --primary --mode 3440x1440 --pos 1440x863 --rotate normal --output DP-1-3 --mode 2560x1440 --pos 0x0 --rotate left
+sleep 3
+nitrogen --restore
--- dot_screenlayout/private_executable_30_work.sh
+++ dot_screenlayout/private_executable_30_work.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --mode 1920x1080 --pos 0x360 --rotate normal --output DP1 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
-sleep 1
+xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --primary --mode 2560x1440 --pos 1920x0 --rotate normal
+sleep 3
 nitrogen --restore
