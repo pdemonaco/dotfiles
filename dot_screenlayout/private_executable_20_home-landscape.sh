10929abb600e21b67bec526af7a0c02fc72083d7
Philip DeMonaco <phil@demona.co>
2021-06-10 10:55:59 -0400

    maint: display and i3 related migration cleanup.

--- dot_screenlayout/private_executable_20_home-landscape.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-xrandr --output DP-0.2 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-0.3 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --off --output HDMI-0 --off --output DP-3 --off --output eDP-1-1 --off
-sleep 1
-nitrogen --restore
