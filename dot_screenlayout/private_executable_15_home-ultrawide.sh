8bd961a035a8e918d19bcfdd7bc4dc72ef6efdba
Philip DeMonaco <phil@demona.co>
2021-06-11 09:35:57 -0400

    maint: remove extra displays
    Having two docks in the same X session created extra devices which
    lingered and needed cleaning up.

--- dot_screenlayout/private_executable_15_home-ultrawide.sh
+++ dot_screenlayout/private_executable_15_home-ultrawide.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output eDP --mode 1920x1080 --pos 3440x360 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --primary --mode 3440x1440 --pos 0x0 --rotate normal --output DisplayPort-6 --off
+xrandr --output eDP --mode 1920x1080 --pos 3440x360 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --primary --mode 3440x1440 --pos 0x0 --rotate normal --output DisplayPort-4 --off
 sleep 1
 nitrogen --restore
