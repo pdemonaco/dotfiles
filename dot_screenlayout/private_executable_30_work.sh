23066e48f2ae93044498e4f44c943a75c0bbb6e6
Philip DeMonaco <phil@demona.co>
2022-02-04 18:36:48 -0500

    maint: monitor change stuff

--- dot_screenlayout/private_executable_30_work.sh
+++ dot_screenlayout/private_executable_30_work.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --primary --mode 2560x1440 --pos 1920x0 --rotate normal
-sleep 3
+xrandr --output eDP --mode 1920x1080 --pos 5120x360 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --mode 2560x1440 --pos 2560x0 --rotate normal --output DisplayPort-3 --off --output DisplayPort-4 --primary --mode 2560x1440 --pos 0x0 --rotate normal
+sleep 1
 nitrogen --restore
